019e0804308584a496daa9ce91ec33ccd6d5e545
akhmetov4 <38395563+akhmetov4@users.noreply.github.com>
2025-01-17 16:19:17 +0500

    feat(api): add root route
    Add dummy root route

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,10 @@ const { getWeather } = require("../controllers/weatherController");
 
 const router = express.Router();
 
+router.get('/', function (req, res) {
+    res.send('Freshtab proxy is ready')
+})
+
 router.get("/air-quality", getAirQuality);
 router.get("/weather", getWeather);
 
